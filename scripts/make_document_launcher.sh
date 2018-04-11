16603829e84028f48a614562e8f8f3cf8f4dcdc0
MaximilienNaveau <maximilien.naveau@gmail.com>
2018-04-11 18:12:25 +0200

    sleep 1s when no file are to be noted, it means no build are necessary

--- scripts/make_document_launcher.sh
+++ scripts/make_document_launcher.sh
@@ -7,13 +7,19 @@ function ctrl_c() {
     exit 1
 }
 
-
 while true ;
 do
     list_files=$(find src/ -name '*.tex')
-    list_files+=" $(find src/ -name '*.bib')"
-    echo "the files to watch are "$list_files
-    inotifywait -e modify $list_files
-    make fast
-    sleep 0.1
+    list_files+=" "$(find src/ -name '*.bib')
+    if [[ -z "${list_files// }" ]]; then
+	# list empty
+#	echo "no files to notify, waiting 1s..."
+	sleep 1
+    else
+	# list not empty
+	echo "the files to watch are "$list_files
+	inotifywait -e modify $list_files
+	make fast
+	sleep 0.1
+    fi
 done
